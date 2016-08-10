8cdf7ed522e3041126d511de6491ee0a20905472
Witoo Harianto <witooh@icloud.com>
2016-08-10 19:48:43 +0700

    add config

--- my_print.py
+++ my_print.py
@@ -4,7 +4,12 @@ from pokemongo_bot.base_task import BaseTask
 class PrintText(BaseTask):
     SUPPORTED_TASK_API_VERSION = 1
 
+    def initialize(self):
+        self.watch_pokemon = self.bot.watch_pokemon
+        self.watch_iv = self.bot.watch_iv
+
     def work(self):
-        print 'PrintText!'
+        print sef.watch_pokemon
+        print sef.watch_iv
         return 'PrintText2'
 
